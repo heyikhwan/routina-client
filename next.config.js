ea0c5dbdd0ebde716a5beded0174ffd3ece16922
heyikhwan <ikhwanmail231000@gmail.com>
2023-12-08 14:55:57 +0700

    setup pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    disable: false, // TODO: kalau udah production nnti di komen
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    }
+});
+
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
